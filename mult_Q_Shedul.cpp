2710adf3835c3734d605bdc76f7ac20a8db56d00
Federico Barbetti <federico.barbetti@correounivalle.edu.co>
2024-10-21 18:16:58 -0500

    se actualizan funciones y se prueban ..van mejorando..

--- mult_Q_Shedul.cpp
+++ mult_Q_Shedul.cpp
@@ -11,18 +11,26 @@
 #include<vector>
 #include<algorithm>
 #include<sstream>
+#include <bits/stdc++.h>
+#include <unistd.h> // For sleep
 
 using namespace std;
 
 std::vector <string> sheduler;               //Variable GLOBAL, es un listado de registros.
 string* d_apuntador=&sheduler[0];       //representa la primera ubicacion en la lista de ejecucion de las tareas.
+stringstream sst;
+
+
+
 
 /**
  * @brief variable GLOBAL
  * Corresponde al archivo de texto con los datos correspodientes a las
  * tareas por ejecutarse.
  */
-string listaDatos="mlq001-a.txt";
+string listaDatos="/home/fede/p_cpp_proyects/examen_parcial_1_SO/parcial_1_SO/mlq001.txt";
+
+//string datos_recibidos;
 
 
 /// @brief
@@ -35,12 +43,12 @@ struct job_ready
     int aa_serie;                         //particularizacion del objeto.
     string a_etiq;                        //representa la etiqueta de la tarea
    
-    string b_burst;                          //representa el Burst time de la tarea
-    string c_arrvt;                          //representa el arrival time de la tarea
-    string d_quev;                           //representa una determinada cola de procesos de tareas
-    string e_prior;                          //representa el indice de la prioridad de ejecucion de una tarea con relacion a las demas tareas
+    int b_burst;                          //representa el Burst time de la tarea
+    int c_arrvt;                          //representa el arrival time de la tarea
+    int d_quev;                           //representa una determinada cola de procesos de tareas
+    int e_prior;                          //representa el indice de la prioridad de ejecucion de una tarea con relacion a las demas tareas
 
-    void set_registro(int ser, string et, string bt, string at, string qv, string prio){
+    void set_registro(int ser, string et, int bt, int at, int qv, int prio){
         aa_serie=ser;
         a_etiq = et;
         b_burst = bt;
@@ -51,8 +59,8 @@ struct job_ready
 };
 
 /**
- * @brief 
- * 
+ * @brief funcion que elimina simbolos no necesarios
+ * para el proceso de los datos
  * @param input 
  * @return std::vector<string> 
  */
@@ -68,20 +76,11 @@ std::vector<string> string_To_array(std::string input)
 
     while (stringReader >> receptor)
     {
+
         str_arreglo.push_back(receptor);
     }
 
     return str_arreglo;
-}
-
-job_ready config_registro(){  // parametros a subir string info_leida, pr_ready pr
-
-    job_ready pr, otro;
-   // pr.set_registro("A", 1, 2, 3, 4);
-
-
-
-    return pr;
 }
 
 
@@ -98,10 +97,10 @@ std::vector<job_ready>orden_por_prioridad(vector<job_ready>cola_registros){
 
     for (int i = 0; i < cola_registros.size(); i++)
     {
-        if ( (cola_registros.at(i).e_prior - ref_priority) >
+        if (     (cola_registros.at(i).e_prior -  ref_priority) >
         (cola_registros.at(i+1).e_prior - ref_priority) )
         {
-            cola_registros.at(i+1)=cola_registros.at(i);
+            cola_registros.at(i+1)=cola_registros.at(i);            //mayor hacia adelante
         }
         
     }
@@ -117,19 +116,25 @@ std::vector<job_ready>orden_por_prioridad(vector<job_ready>cola_registros){
  * 
  * @param ubicacion_de_los_datos 
  */
-std::vector<string>lectura_archivo_txt(const string ubicacion_de_los_datos)
+std::vector<char*>lectura_archivo_txt(const string ubicacion_de_los_datos)
 {
-    vector<string>lineas_recibidas;
+    vector<char*>lineas_recibidas;
 
     string receptorDatos;                   //obj que recepcionara los datos filtrados
-
-    std::string auxiliar_line;
-
-    char aauuxx[80];
     
-    char salvador[100];                   //auxiliar de proceso recepciona un string
+    string esperanza="";
+
+    char receptor[] ="";
     
-    char select_info;                       //auxiliar para validar renglon a leer
+    char select_info; 
+    
+    char destino[100] ="";                     //auxiliar para validar renglon a leer
+    
+    char b_destino[100]="";
+
+    char sx_elemento[20];                       //cadena hasta el ";"
+
+    int zt_cuenta=0;                            //cuantnos simbolos fueron leidos la ultima vez
     
     std::ifstream lector(ubicacion_de_los_datos);       //obj stream que lee datos del archivo de texto
 
@@ -137,94 +142,109 @@ std::vector<string>lectura_archivo_txt(const string ubicacion_de_los_datos)
     {
         do
         {
-            select_info = lector.peek();
-
-            if (!lector.fail())
-            { 
-                // char caracter_final;
-
-                // caracter_final = lector.get();
-
-                getline(lector, auxiliar_line);
+            zt_cuenta=lector.gcount();
+            select_info=lector.peek();
+            char detenedor=lector.get();
 
 
 
-                if ((select_info>=65 && select_info<=90) || (select_info>=97 && select_info<=122))// ASCII value range
-                {
+            int limite =10;
+            int cuenta_renglones=0;
+            
 
-                    lineas_recibidas.push_back(auxiliar_line);
+            if (!lector.fail())                 //aqui empieza realmente el buffer
+            {
+
+                lector.getline(destino, 99, '\n'); 
+               
+                lector.get(sx_elemento, 20, ';');
+
+                    if (destino!=NULL && select_info !=35)
+                    {
+                            
+                                int numerador = 0;
 
 
-                }
-                
+
+                                if(detenedor!='\n' && select_info !=35  )
+                                {
+                                    
+                                    cout<<" soy destino "<<destino
+                                   // <<" mi posicion es; "<<numerador
+                                    <<"\nsoy sx elemento "<<sx_elemento
+                                    <<endl;
+                                    numerador++;
+
+                                    // strcat(receptor, destino);
+                                }
+
+                    }
+
             }
+
             
         } while (!lector.fail());
         
+        
     }
     else{
-        cout<<"problemas para lectura de datos\n";
+
+        cout<<"no se pudo abrir el stream"<<endl;
     }
 
-    lector.close();
+    //veficaciones
 
-    //verificacion del proceso 
-
-    for(std::string dentro : lineas_recibidas){
-        cout<<"en verificacion "<<dentro<<endl;
+    for (int i = 0; i < sizeof(receptor); i++)
+    {
+        cout<<receptor[i]<<endl;
     }
+
 
     return lineas_recibidas;                                            
 }
 
-std::vector<job_ready>tarea_en_espera(std::vector<string>l_recibidas){
 
-    std::vector<string>unico;
+job_ready config_job(string datos){
 
-    std::vector<job_ready>jobs;
 
-    int serial = 100;
 
-    for(string rec : l_recibidas){
 
-        unico = string_To_array(rec);
+    int contador = 0;
+   // vector<char>paraDatos;
+    char misdatos[6];
+    misdatos[5]='\0';
 
-        for(job_ready jr: jobs){
-
-            jr.aa_serie=serial;
-
-            serial++;
-
-            for (int i = 0; i < unico.size(); i++)
-            {
-                jr.a_etiq = unico.at(0);
-
-                jr.b_burst = unico.at(1);
-
-                jr.c_arrvt = unico.at(2);
-
-                jr.d_quev = unico.at(3);
-
-                jr.e_prior=unico.at(4);
-                
-            }
-
-            jobs.push_back(jr);
-            
-
+    for(char ch : datos){
+        if( ( (int)ch>47 && (int)ch<58 ) || ( (int)ch>96 && (int)ch<123 ) || ( (int)ch>64 && (int)ch<91 )  ) {
+           // paraDatos.push_back(ch);
+            misdatos[contador]=ch;
+            contador++;
+            cout<<" o soy letra o numero ..soy "<<ch<<" contador es "<<contador<<endl;
 
         }
-
+        else{
+            cout<<ch<<" no soy ni letra ni numero \n";
+        } 
     }
+    
+    job_ready limpio_jr;
 
-    for(job_ready rr : jobs){
+    limpio_jr.a_etiq=misdatos[0];
+    limpio_jr.b_burst=misdatos[1] ;
+    limpio_jr.c_arrvt=misdatos[2];
+    limpio_jr.d_quev=misdatos[3];
+    limpio_jr.e_prior=misdatos[4];
 
-        cout<<"soy el bt"<<rr.b_burst<<endl;
-    }
+    //prueba
+    cout<<"\nSoy limpio mi etiqueta es: "<<limpio_jr.a_etiq
+    <<"\nSoy limpio mi burst time es: "<<limpio_jr.b_burst
+    <<"\nSoy limpio mi arrival time es: "<<limpio_jr.c_arrvt
+    <<"\nSoy limpio mi queve es: "<<limpio_jr.d_quev
+    <<"\nSoy limpio mi prioridad es: "<<limpio_jr.e_prior
+    <<endl;
 
-
-
-
+    return limpio_jr;
+    
 }
 
 
@@ -233,12 +253,17 @@ int main(int argc, char const *argv[])
 {
     //prueba funcion de lectura de archivos
 
-    string listaPrueba="mlq001-a.txt";
+    job_ready jr1;                                                         //instania
 
-    lectura_archivo_txt(listaPrueba);
-
+    string listaPrueba="/home/fede/p_cpp_proyects/examen_parcial_1_SO/parcial_1_SO/mlq001.txt";
     
+    vector <char*> el_probador;
 
+    std::vector<job_ready>cola_trb;
+
+    el_probador=lectura_archivo_txt(listaPrueba);  //strings con puntuacion 
+
+    cout<<"size de el vector de salida "<<sizeof(el_probador)<<endl;
 
 
 
@@ -247,4 +272,4 @@ int main(int argc, char const *argv[])
     cout<<"bendito Dios...\n";                     //agradecimiento a DIOS.
 
     return 0;
-}
+}   
